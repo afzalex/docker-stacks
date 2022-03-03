7759711cfbd605c742061b7d93fbe2f61c46ed1b
Mohammad Afzal <mafzal@calibo.com>
2022-03-03 17:29:07 +0530

    Added profiling and kafka host listeners

--- infrastructure/kafka/kafka-starter.sh
+++ infrastructure/kafka/kafka-starter.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
- if [ -z ${KAFKA_CFG_ZOOKEEPER_CONNECT} ]; then 
-    echo "KAFKA_CFG_ZOOKEEPER_CONNECT environment not set"
+ if [ -z ${KAFKA_ZOOKEEPER_CONNECT} ]; then 
+    echo "KAFKA_ZOOKEEPER_CONNECT environment not set"
     exit 1
 fi
 
 
 count=0
 while true; do 
-    zookeeperConnectionOutput=$(curl -sS "${KAFKA_CFG_ZOOKEEPER_CONNECT}" 2>&1); 
+    zookeeperConnectionOutput=$(curl -sS "${KAFKA_ZOOKEEPER_CONNECT}" 2>&1); 
     if [ ! "${zookeeperConnectionOutput}" = 'curl: (52) Empty reply from server' ]; then 
         count=$(( $count + 1 ))
         echo -e "\033[31mzookeeper is not available at ... ${count}\033[0m"
         sleep 1
     else 
-        echo "zookeeper found at ${KAFKA_CFG_ZOOKEEPER_CONNECT}"
+        echo "zookeeper found at ${KAFKA_ZOOKEEPER_CONNECT}"
         break
     fi
 done
 
+/bin/cat /usr/local/kafka/config-templates/server.properties | /usr/bin/envsubst > /usr/local/kafka/config/server.properties
 
-/kafka/bin/kafka-server-start.sh /kafka/config/server.properties
+/usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties
