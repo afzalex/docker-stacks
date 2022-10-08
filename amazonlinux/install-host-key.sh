4dbb47fe091b13bf41c45af80ca4153f9c8a1739
Mohammad Afzal <afzalex.store@gmail.com>
2022-10-09 01:06:18 +0530

    Updated amazonlinux

--- amazonlinux/install-host-key.sh
+++ amazonlinux/install-host-key.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "Installing host ssh keys in docker ec2"
 docker cp ~/.ssh/id_rsa.pub ec2:/tmp
-docker exec -t ec2 /bin/bash -c 'cat /tmp/id_rsa.pub >> ~/.ssh/authorized_keys'
+docker exec -it ec2 /bin/bash -c 'cat /tmp/id_rsa.pub >> ~/.ssh/authorized_keys'
+docker exec -it ec2 touch FLAG_HOST_AUTHKEY_INSTALLED
