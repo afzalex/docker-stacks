2c7c23736c01e5c937f3927fd2e35b503ce7c26f
Mohammad Afzal <afzalex.store@gmail.com>
2024-06-20 21:56:01 +0530

    Updated mysql

---
+++ mysql/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Runner for mysql
+
+source ../run-preprocess.sh.tpl
+
+docker run --name ${IMAGE_NAME} -it \
+    --network "${NETWORK_NAME}" \
+    --env-file "public.env" \
+    --env-file ".env" \
+    -v ./.data/var/lib/mysql:/var/lib/mysql \
+    -p ${PORT_MAPPING}:3306 --rm \
+    ${CONTAINER_NAME}
