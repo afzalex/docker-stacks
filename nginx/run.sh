3ca8bcd4938f6948eca217afd0309335a5ed301d
Mohammad Afzal <afzalex.store@gmail.com>
2024-06-25 21:58:03 +0530

    Updated nginx

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+source prerun.sh
+
 export ENVRIONMENT_PREFIX="FZNGINX"
 
 source ../run-preprocess.sh.tpl
-
 
 docker run --name ${IMAGE_NAME} -it \
     --network "${NETWORK_NAME}" \
     --env-file "public.env" \
     --env-file <(env | grep "$ENVRIONMENT_PREFIX") \
-    -p 80:80 -p 443:443 --rm \
+    -p 80:80 -p 443:443 --rm -d \
     ${CONTAINER_NAME}
